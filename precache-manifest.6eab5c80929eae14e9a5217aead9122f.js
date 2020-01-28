08d0ddb284863ce65507a48dc969d0282be21e97
joaolima <joaolimafonseca2@gmail.com>
2020-01-28 17:37:45 -0300

    tomara que nao de me***

---
+++ precache-manifest.6eab5c80929eae14e9a5217aead9122f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11460c125a5328f4afc788fb48a3a13c",
+    "url": "/CNUC/index.html"
+  },
+  {
+    "revision": "17fcf2ca13b8434a86f0",
+    "url": "/CNUC/static/css/2.d3ff1f59.chunk.css"
+  },
+  {
+    "revision": "d240e848b8ba62b0f29e",
+    "url": "/CNUC/static/css/main.fd730900.chunk.css"
+  },
+  {
+    "revision": "17fcf2ca13b8434a86f0",
+    "url": "/CNUC/static/js/2.3ba72274.chunk.js"
+  },
+  {
+    "revision": "467df5873c07603929e81f404443691e",
+    "url": "/CNUC/static/js/2.3ba72274.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d240e848b8ba62b0f29e",
+    "url": "/CNUC/static/js/main.2594b051.chunk.js"
+  },
+  {
+    "revision": "269e147fd699cc398900",
+    "url": "/CNUC/static/js/runtime-main.344cd309.js"
+  },
+  {
+    "revision": "f93dd1317baa70bcf58462a675be41e3",
+    "url": "/CNUC/static/media/04.f93dd131.jpg"
+  },
+  {
+    "revision": "a193d17273797d6f282448573173117e",
+    "url": "/CNUC/static/media/cnuc.a193d172.png"
+  },
+  {
+    "revision": "01523d0c9916677183a75d127b1a1186",
+    "url": "/CNUC/static/media/img02.01523d0c.png"
+  },
+  {
+    "revision": "0bf06dcca662911c94854ebc9d2d01ef",
+    "url": "/CNUC/static/media/logoMMA.0bf06dcc.png"
+  }
+]);
